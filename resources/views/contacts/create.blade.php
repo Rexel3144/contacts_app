e9197c52a35680ba32ab586b05d41985e9610deb
user <user@user-desktop>
2017-04-26 15:24:30 +0300

    Changed a form.

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -1,10 +1,84 @@
 @extends('layouts.app')
 @section('content')
-<form method="POST" action="{{route('contact.store')}}">
-    {{csrf_field()}}
-    Name<input type="text" name="name"><br/>
-    Company<input type="text" name="company"><br/>
-    Phone<input type="text" name="phone"><br/>
-    <input type="submit">
-</form>
+<div class="container">
+    <div class="row">
+        <form class="form-horizontal" method="POST" action="{{route('contact.store')}}">
+            {{csrf_field()}}
+            <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                <label for="name" class="col-md-4 control-label">Name*</label>
+                <div class="col-md-6">
+                    <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                    @if ($errors->has('name'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('name') }}</strong>
+                    </span>
+                    @endif
+                </div>
+            </div>
+            <div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
+                <label for="phone" class="col-md-4 control-label">Phone*</label>
+                <div class="col-md-6">
+                    <input id="email" type="text" class="form-control" name="phone" value="{{ old('phone') }}" required>
+                    @if ($errors->has('phone'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('phone') }}</strong>
+                    </span>
+                    @endif
+                </div>
+            </div>
+            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                <label for="email" class="col-md-4 control-label">Email</label>
+                <div class="col-md-6">
+                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
+                    @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                    @endif
+                </div>
+            </div>
+            <div class="form-group{{ $errors->has('address') ? ' has-error' : '' }}">
+                <label for="address" class="col-md-4 control-label">Address</label>
+                <div class="col-md-6">
+                    <input id="address" type="text" class="form-control" name="address" value="{{ old('address') }}">
+                    @if ($errors->has('address'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('address') }}</strong>
+                    </span>
+                    @endif
+                </div>
+            </div>
+            <div class="form-group{{ $errors->has('company') ? ' has-error' : '' }}">
+                <label for="company" class="col-md-4 control-label">Company</label>
+                <div class="col-md-6">
+                    <input id="company" type="text" class="form-control" name="company" value="{{ old('company') }}">
+                    @if ($errors->has('company'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('company') }}</strong>
+                    </span>
+                    @endif
+                </div>
+            </div>
+            <div class="form-group{{ $errors->has('birthday') ? ' has-error' : '' }}">
+                <label for="birthday" class="col-md-4 control-label">Birthday</label>
+                <div class="col-md-6">
+                    <input id="birthday" type="date" class="form-control" name="birthday" value="{{ old('birthday') }}">
+                    @if ($errors->has('birthday'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('birthday') }}</strong>
+                    </span>
+                    @endif
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-6 col-md-offset-4">
+                    <button type="submit" class="btn btn-primary">
+                        Create
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
 @endsection
