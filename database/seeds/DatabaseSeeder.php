f2902a76b38f40dae38a9620f2051d556a405756
user <user@user-desktop>
2017-04-26 11:38:13 +0300

    Create a seeder for database.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,15 +2,19 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
+    public function run() {
+        factory('App\User', 5)->create()->each(function($user) {
+            for ($i = 0; $i < rand(1, 6); $i++) {
+                $user->contacts()->save(factory('App\Contact')->make());
+            }
+        });
     }
+
 }
