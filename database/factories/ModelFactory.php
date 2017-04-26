ef9d1c52472fceefa9e01ad2b83be3e98d32afaa
user <user@user-desktop>
2017-04-26 11:36:52 +0300

    Created factories for contacts and companies.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -16,9 +16,31 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
+        'name' => $faker->firstName,
+        'lastname' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Contact::class, function (Faker\Generator $faker) {
+
+
+    return [
+        'company_id' => factory('App\Company')->create()->id,
+        'name' => $faker->name,
+        'phone' => $faker->phoneNumber,
+        'address' => $faker->address,
+        'birthday' => $faker->date(),
+        'email' => $faker->unique()->safeEmail,
+    ];
+});
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Company::class, function (Faker\Generator $faker) {
+
+
+    return [
+        'name' => $faker->company,
     ];
 });
