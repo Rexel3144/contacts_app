9a79058659015daf35bc829b444e92510c4384ed
user <user@user-desktop>
2017-04-26 20:27:10 +0300

    Created an edit form for each possible value.

---
+++ resources/views/contacts/parts/birthday.php
@@ -0,0 +1,8 @@
+<div class="edit-cell" data-contact-id="{{$id}}" data-source="{{$source}}">
+    <div class="edit-input">
+        <input   type="date" value="{{$value}}">
+    </div>
+    <div class="confirm">
+        <span class="glyphicon glyphicon-ok"></span>
+    </div>
+</div>
